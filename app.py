8708307172adb4a63764bcf79e8fd7ab22cabb30
Siddy <siddy.p@icloud.com>
2026-04-25 04:18:21 +0100

    chore: configure for render deployment (postgres, env vars, gunicorn port)

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'drsos-dev-secret-2024-change-in-prod')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///drsos.db'
+db_url = os.environ.get('DATABASE_URL', 'sqlite:///drsos.db')
+if db_url.startswith('postgres://'):
+    db_url = db_url.replace('postgres://', 'postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
